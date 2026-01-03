8bb4fc4fdfc5ffd183fc8c64357e9d69d4b39719
Afshan Anjum <168090499+afshan-anjum@users.noreply.github.com>
2026-01-04 02:31:09 +0500

    Fix url and baseUrl for Vercel

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/afshan-anjum.github.io',
+  url: 'https://physical-ai-humanoid-robbotic-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-AI-Humanoid-Robbotic-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
